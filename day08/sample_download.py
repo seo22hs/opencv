f68b8f24a501cfda82f3e7501f620844e2045e25
seo22hs <lsc584727@gmail.com>
2026-04-08 15:58:21 +0900

    feat : opencv, insight 2종 sample

--- day08/sample_download.py
+++ day08/sample_download.py
@@ -1,12 +1,23 @@
 import urllib.request
 import os
-import cv2 as cv
 
-def get_sample(filename, repo='opencv'):
-    if not os.path.exists(filename):
-        if repo == 'insightbook':
-            url = f"https://raw.githubusercontent.com/dltpdn/insightbook.opencv_project_python/master/img/{filename}"
-        else:
-            url = f"https://raw.githubusercontent.com/opencv/opencv/master/samples/data/{filename}"
-        urllib.request.urlretrieve(url, filename)
+def get_sample(filename):
+    if os.path.exists(filename):
+        return filename
+
+    # 1순위: OpenCV 공식 저장소 시도
+    opencv_url = f"https://raw.githubusercontent.com/opencv/opencv/master/samples/data/{filename}"
+    # 2순위: 인사이트북 저장소 시도
+    insight_url = f"https://raw.githubusercontent.com/dltpdn/insightbook.opencv_project_python/master/img/{filename}"
+
+    for url in [opencv_url, insight_url]:
+        try:
+            print(f"Trying to download from: {url}")
+            urllib.request.urlretrieve(url, filename)
+            print(f"Successfully downloaded {filename}")
+            return filename
+        except Exception:
+            continue # 실패하면 다음 URL 시도
+
+    print(f"Error: {filename}을 어느 저장소에서도 찾을 수 없습니다.")
     return filename
